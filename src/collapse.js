2534da8ffbb54e5a90782966b85335309400da41
AndyHsiao0223 <Andy90.2.23@gmail.com>
2023-09-19 21:03:23 +0800

    add js

---
+++ src/collapse.js
@@ -0,0 +1,44 @@
+const btntog1 = () => {
+    const t = document.getElementById('ans-1');
+    if (t.style.display == 'none') {
+        t.style.display = 'inline';
+    } else {
+        t.style.display = 'none';
+    }
+}
+
+const btntog2 = () => {
+    const t = document.getElementById('ans-2');
+    if (t.style.display == 'none') {
+        t.style.display = 'inline';
+    } else {
+        t.style.display = 'none';
+    }
+}
+
+const btntog3 = () => {
+    const t = document.getElementById('ans-3');
+    if (t.style.display == 'none') {
+        t.style.display = 'inline';
+    } else {
+        t.style.display = 'none';
+    }
+}
+
+const btntog4 = () => {
+    const t = document.getElementById('ans-4');
+    if (t.style.display == 'none') {
+        t.style.display = 'inline';
+    } else {
+        t.style.display = 'none';
+    }
+}
+
+const btntog5 = () => {
+    const t = document.getElementById('ans-5');
+    if (t.style.display == 'none') {
+        t.style.display = 'inline';
+    } else {
+        t.style.display = 'none';
+    }
+}
